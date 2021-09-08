47342d339f1349a90079238827c032ac67b2680a
lukaszpiera <pieralukasz@gmail.com>
2021-09-08 13:58:19 +0200

    change cloud template names

--- cli/cli.js
+++ cli/cli.js
@@ -1,17 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.cli = void 0;
+const path = require("path");
 const child_process_1 = require("child_process");
 function cli() {
-    console.log(__dirname);
-    if (__dirname) {
-        const path = __dirname.split("@")[1];
-        console.log(path);
-        child_process_1.spawn(`schematics @${path}packages/collection.json:app`, {
-            stdio: "inherit",
-            shell: true,
-        });
+    let directory;
+    if (__dirname.includes("@")) {
+        // npx via github
+        directory = `@${path.join(__dirname, "..").split("@")[1]}`;
     }
+    else {
+        // installed project
+        directory = `${path.join(__dirname, "..")}/`;
+    }
+    child_process_1.spawn(`schematics ${directory}packages/collection.json:app`, {
+        stdio: "inherit",
+        shell: true,
+    });
 }
 exports.cli = cli;
 //# sourceMappingURL=cli.js.map
--- cli/cli.ts
+++ cli/cli.ts
@@ -1,16 +1,19 @@
+import * as path from "path";
 import { spawn } from "child_process";
 
 export function cli() {
-  console.log(__dirname);
+  let directory;
 
-  if (__dirname) {
-    const path = __dirname.split("@")[1];
-
-    console.log(path);
-
-    spawn(`schematics @${path}packages/collection.json:app`, {
-      stdio: "inherit",
-      shell: true,
-    });
+  if (__dirname.includes("@")) {
+    // npx via github
+    directory = `@${path.join(__dirname, "..").split("@")[1]}`;
+  } else {
+    // installed project
+    directory = `${path.join(__dirname, "..")}/`;
   }
+
+  spawn(`schematics ${directory}packages/collection.json:app`, {
+    stdio: "inherit",
+    shell: true,
+  });
 }
