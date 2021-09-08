ac187458250b6b6543dd6b3e35ee1d3dc180c09d
Jacek Kościesza <jacek.kosciesza@gmail.com>
2021-09-08 14:30:19 +0200

    tests: check sgenerated files structure (#28)

--- packages/web/index_spec.ts
+++ packages/web/index_spec.ts
@@ -4,6 +4,28 @@ import * as path from "path";
 
 const collectionPath = path.join(__dirname, "../collection.json");
 
+export const expectedFiles = [
+  "/web/.gitignore",
+  "/web/README.md",
+  "/web/package.json",
+  "/web/tsconfig.json",
+  "/web/public/favicon.ico",
+  "/web/public/index.html",
+  "/web/public/logo192.png",
+  "/web/public/logo512.png",
+  "/web/public/manifest.json",
+  "/web/public/robots.txt",
+  "/web/src/App.css",
+  "/web/src/App.test.tsx",
+  "/web/src/App.tsx",
+  "/web/src/index.css",
+  "/web/src/index.tsx",
+  "/web/src/logo.svg",
+  "/web/src/react-app-env.d.ts",
+  "/web/src/reportWebVitals.ts",
+  "/web/src/setupTests.ts",
+];
+
 describe("web", () => {
   it("works", async () => {
     const runner = new SchematicTestRunner("schematics", collectionPath);
@@ -11,6 +33,6 @@ describe("web", () => {
       .runSchematicAsync("web", { name: "janush-app" }, Tree.empty())
       .toPromise();
 
-    expect(tree.files.length).toEqual(19);
+    expect(tree.files).toEqual(expectedFiles);
   });
 });
