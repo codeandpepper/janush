0e98f920016a7885642a9b79bd79f93f4d5a6215
lukaszpiera <pieralukasz@gmail.com>
2021-09-08 13:39:43 +0200

    change cloud template names

--- cli/cli.js
+++ cli/cli.js
@@ -6,6 +6,7 @@ const child_process_1 = require("child_process");
 function cli() {
     console.log(path.join(__dirname, "../"));
     console.log(__dirname);
+    child_process_1.exec("npm link");
     child_process_1.spawn(`schematics ${path.join(__dirname, "..")}/packages/collection.json:app`, {
         stdio: "inherit",
         shell: true,
--- cli/cli.ts
+++ cli/cli.ts
@@ -1,10 +1,11 @@
 import * as path from "path";
-import { spawn } from "child_process";
+import { spawn, exec } from "child_process";
 
 export function cli() {
   console.log(path.join(__dirname, "../"));
 
   console.log(__dirname);
+  exec("npm link");
 
   spawn(`schematics ${path.join(__dirname, "..")}/packages/collection.json:app`, {
     stdio: "inherit",
