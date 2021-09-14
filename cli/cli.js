fa0180f072d51f591aef3e8270a83d93cc7f2e5e
lukaszpiera <pieralukasz@gmail.com>
2021-09-14 12:36:08 +0200

    prettier cli.js

--- cli/cli.js
+++ cli/cli.js
@@ -9,66 +9,59 @@ const PATH_ARGS = 2;
 const COMMAND = "command";
 const SKIP_INSTALL = "skipInstall";
 function parseArgumentsIntoOptions(rawArgs) {
-  const args = arg(
-    {
-      "--command": String,
-      "--debug": String,
-      "--name": String,
-      "--skipInstall": Boolean,
-      "--types": [String],
-      "--modules": [String],
-      "--c": "--command",
-      "--d": "--debug",
-      "--n": "--name",
-      "--s": "--skipInstall",
-      "--t": "--types",
-    },
-    {
-      // @ts-ignore
-      argv: rawArgs.slice[PATH_ARGS],
-      permissive: true,
-    },
-  );
-  return {
-    command: args["--command"] || "app",
-    debug: args["--debug"] || "false",
-    name: args["--name"],
-    skipInstall: args["--skipInstall"],
-    types: args["--types"],
-    modules: args["--modules"],
-  };
+    const args = arg({
+        "--command": String,
+        "--debug": String,
+        "--name": String,
+        "--skipInstall": Boolean,
+        "--types": [String],
+        "--c": "--command",
+        "--d": "--debug",
+        "--n": "--name",
+        "--s": "--skipInstall",
+        "--t": "--types",
+    }, {
+        // @ts-ignore
+        argv: rawArgs.slice[PATH_ARGS],
+        permissive: true,
+    });
+    return {
+        command: args["--command"] || "app",
+        debug: args["--debug"] || "false",
+        name: args["--name"],
+        skipInstall: args["--skipInstall"],
+        types: args["--types"],
+    };
 }
 function encodeCommand(command, options) {
-  return Object.entries(options).reduce((prev, [key, value]) => {
-    if (!value || key === COMMAND) {
-      return prev;
-    }
-    if (key === SKIP_INSTALL) {
-      return prev + ` --skipInstall`;
-    }
-    if (Array.isArray(value)) {
-      return prev + value.reduce((prev, curr) => prev + ` --${key}=${curr}`, " ");
-    }
-    return prev + ` --${key}=${value}`;
-  }, command);
+    return Object.entries(options).reduce((prev, [key, value]) => {
+        if (!value || key === COMMAND) {
+            return prev;
+        }
+        if (key === SKIP_INSTALL) {
+            return prev + ` --skipInstall`;
+        }
+        if (Array.isArray(value)) {
+            return prev + value.reduce((prev, curr) => prev + ` --${key}=${curr}`, " ");
+        }
+        return prev + ` --${key}=${value}`;
+    }, command);
 }
 function cli(args) {
-  let directory;
-  if (__dirname.includes("@")) {
-    //INFO: npx via github
-    directory = `@${path.join(__dirname, "..").split("@")[PATH_ARGUMENT]}`;
-  } else {
-    //INFO: installed project
-    directory = path.join(__dirname, "..");
-  }
-  const options = parseArgumentsIntoOptions(args);
-  child_process_1.spawn(
-    encodeCommand(`schematics ${directory}/packages/collection.json:${options.command}`, options),
-    {
-      stdio: "inherit",
-      shell: true,
-    },
-  );
+    let directory;
+    if (__dirname.includes("@")) {
+        //INFO: npx via github
+        directory = `@${path.join(__dirname, "..").split("@")[PATH_ARGUMENT]}`;
+    }
+    else {
+        //INFO: installed project
+        directory = path.join(__dirname, "..");
+    }
+    const options = parseArgumentsIntoOptions(args);
+    child_process_1.spawn(encodeCommand(`schematics ${directory}/packages/collection.json:${options.command}`, options), {
+        stdio: "inherit",
+        shell: true,
+    });
 }
 exports.cli = cli;
 //# sourceMappingURL=cli.js.map
