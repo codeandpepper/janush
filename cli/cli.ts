78aa31ef4125e6864f7859652a348a275a64b0e6
Damian Cop <dcop.dev@gmail.com>
2021-09-14 08:27:16 +0200

    build: run from npx (#3) (#35)
    * build: implement project run using npx (#3)
    
    * build: improve run-from-npx code (#3)
    
    * build: fix package-lock (#3)
    
    * build: fix package-lock (#3)
    
    * build: fix package-lock (#3)
    
    * build: cli.js fix (#3)
    
    * build: remove console.log (#3)
    
    * build: add GitHub step for linking (#3)
    
    * build: add README.md (#3)
    
    * build: add README.md pt. 2 (#3)
    
    * build: fix CR (#3)
    
    * build: fix problem with array values on linux/win (#3)
    
    Co-authored-by: Damian Cop <d.cop@codeandpepper.com>

---
+++ cli/cli.ts
@@ -0,0 +1,80 @@
+import * as path from "path";
+import * as arg from 'arg';
+import { spawn } from "child_process";
+
+const PATH_ARGUMENT = 1;
+const PATH_ARGS = 2;
+const COMMAND = 'command';
+const SKIP_INSTALL = 'skipInstall';
+
+interface Options {
+  //INFO: Change 'command' to more suitable name for schematics' type
+  command?: string;
+  debug?: string;
+  name?: string;
+  skipInstall?: boolean;
+  types?: string[];
+}
+
+function parseArgumentsIntoOptions(rawArgs: string[]): Options {
+  const args = arg(
+    {
+      '--command': String,
+      '--debug': String,
+      '--name': String,
+      '--skipInstall': Boolean,
+      '--types': [String],
+      '--c': '--command',
+      '--d': '--debug',
+      '--n': '--name',
+      '--s': '--skipInstall',
+      '--t': '--types',
+    },
+    {
+      // @ts-ignore
+      argv: rawArgs.slice[PATH_ARGS],
+      permissive: true,
+    }
+  );
+  return {
+    command: args['--command'] || "app",
+    debug: args['--debug'] || "false",
+    name: args['--name'],
+    skipInstall: args['--skipInstall'],
+    types: args['--types'],
+  }
+}
+
+function encodeCommand(command: string, options: Options) {
+  return Object.entries(options).reduce((prev, [key, value]) => {
+    if (!value || key === COMMAND) {
+      return prev;
+    }
+    if (key === SKIP_INSTALL) {
+      return prev + ` --skipInstall`;
+    }
+    if (Array.isArray(value)) {
+      return prev + value.reduce((prev, curr) => prev + ` --${key}=${curr}`, ' ');
+    }
+    return prev + ` --${key}=${value}`;
+  }, command)
+}
+
+export function cli(args: string[]) {
+  let directory;
+
+  if (__dirname.includes("@")) {
+    //INFO: npx via github
+    directory = `@${path.join(__dirname, "..").split("@")[PATH_ARGUMENT]}`;
+  } else {
+    //INFO: installed project
+    directory = path.join(__dirname, "..");
+  }
+
+  const options = parseArgumentsIntoOptions(args);
+
+  spawn(encodeCommand(`schematics ${directory}/packages/collection.json:${options.command}`, options), {
+    stdio: "inherit",
+    shell: true,
+  });
+}
