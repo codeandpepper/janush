a5e47e70587af3d1a0731ae3fc3e0f5c59715799
Przemysław Rząsa <przemyslawrzasa@Przemysaws-MacBook-Pro.local>
2021-12-27 08:43:12 +0100

    Deployment to dedicated repo implementation

---
+++ schematics/cloud/ci/index.ts
@@ -0,0 +1,39 @@
+import { strings } from "@angular-devkit/core";
+import {
+  apply,
+  applyTemplates,
+  chain,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  schematic,
+  SchematicContext,
+  Tree,
+  url,
+} from "@angular-devkit/schematics";
+
+import { Schematic } from "@enums/Schematic";
+import { readJanushJSON } from "@janush-schematics/utility/janush-json";
+
+export const prepareEnvironment = (options: any): Rule => {
+  return async (tree: Tree, _context: SchematicContext) => {
+    const janushFile = readJanushJSON(tree);
+
+    options.name = strings.dasherize(janushFile.name);
+
+    return chain([
+      mergeWith(
+        apply(url("./files"), [
+          applyTemplates({
+            ...options,
+            ...strings,
+          }),
+          move(Schematic.CLOUD),
+        ]),
+        MergeStrategy.Overwrite
+      ),
+      schematic("apply-prettier", {}),
+    ]);
+  };
+};
--- schematics/cloud/template/schema.ts
+++ schematics/cloud/template/schema.ts
@@ -3,6 +3,6 @@ import { Schema as AppSchema } from "@janush-schematics/application/schema";
 
 export interface Schema extends Partial<AppSchema> {
   name: string;
-  skipInstall: boolean;
   modules: Module[];
+  skipInstall: boolean;
 }
