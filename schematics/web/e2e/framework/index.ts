3de1f5bca4e742f5b90c6c1e18ff5303380096f0
Przemysław <przemyslawrzasa@Przemysaws-MacBook-Pro.local>
2021-12-30 11:15:43 +0100

    E2E auth files only if auth selected

--- schematics/web/e2e/framework/index.ts
+++ schematics/web/e2e/framework/index.ts
@@ -14,8 +14,9 @@ import {
 } from "@angular-devkit/schematics";
 
 import { WEB_PACKAGE_JSON_PATH } from "@consts/index";
-import { E2ERunner } from "@enums/Module";
+import { E2ERunner, Module } from "@enums/Module";
 import { Schematic } from "@enums/Schematic";
+import { stubArg } from "@janush-schematics/utility/stubArg/stubArg";
 import { addPackageJsonDependency } from "@schematics/angular/utility/dependencies";
 import {
   e2eCypressDependencies,
@@ -45,15 +46,29 @@ export const e2eFrameworkGenerator = (options: Schema): Rule => {
             ]),
             MergeStrategy.Overwrite
           ),
-          mergeWith(
-            apply(url("./files/shared/selectors"), [
-              applyTemplates({
-                ...options,
-                ...strings,
-              }),
-              move(`${Schematic.WEB}/cypress/support/selectors`),
-            ])
-          ),
+          options.modules.includes(Module.AUTHENTICATION)
+            ? mergeWith(
+                apply(url("./files/shared/selectors"), [
+                  applyTemplates({
+                    ...options,
+                    ...strings,
+                  }),
+                  move(`${Schematic.WEB}/cypress/support/selectors`),
+                ])
+              )
+            : stubArg,
+          options.modules.includes(Module.AUTHENTICATION)
+            ? mergeWith(
+                apply(url("./files/cypress/authentication"), [
+                  applyTemplates({
+                    ...options,
+                    ...strings,
+                  }),
+                  move(`${Schematic.WEB}/cypress`),
+                ]),
+                MergeStrategy.Overwrite
+              )
+            : stubArg,
           packageJsonCypressExtender,
           schematic("apply-prettier", {}),
         ]);
@@ -65,6 +80,15 @@ export const e2eFrameworkGenerator = (options: Schema): Rule => {
 
         return chain([
           mergeWith(
+            apply(url("./files/shared/selectors"), [
+              applyTemplates({
+                ...options,
+                ...strings,
+              }),
+              move(`${Schematic.WEB}/playwright/page-objects/selectors`),
+            ])
+          ),
+          mergeWith(
             apply(url("./files/playwright"), [
               applyTemplates({
                 ...options,
@@ -73,15 +97,6 @@ export const e2eFrameworkGenerator = (options: Schema): Rule => {
               move(`${Schematic.WEB}/playwright`),
             ]),
             MergeStrategy.Overwrite
-          ),
-          mergeWith(
-            apply(url("./files/shared/selectors"), [
-              applyTemplates({
-                ...options,
-                ...strings,
-              }),
-              move(`${Schematic.WEB}/playwright/page-objects/selectors`),
-            ])
           ),
           packageJsonPlaywrightExtender,
           tsConfigPlaywrightExtender,
