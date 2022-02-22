42821a17927fa161f3223d9f52d6ddd58881e0b6
Oskar Zaremba <o.zaremba@codeandpepper.com>
2022-02-22 14:16:00 +0100

    feature: Added --help option for Janush command (#120)

---
+++ schematics/utility/generateHelpFromSchema.ts
@@ -0,0 +1,54 @@
+import { colors } from "@angular/cli/utilities/color";
+import readJsonFile from "./readJsonFile";
+
+type CommandHelp = {
+  command: string;
+  alias?: string;
+  description: string;
+};
+
+type Property = {
+  aliases?: string[];
+  description: string;
+};
+
+type Properties = {
+  [propery: string]: Property;
+};
+
+type Schema = {
+  properties: Properties;
+};
+
+const generateHelpFromSchema = (relativePath: string): CommandHelp[] => {
+  const schemaJsonContent: Schema = readJsonFile(relativePath);
+  const schemaProperties = schemaJsonContent.properties;
+
+  return Object.keys(schemaProperties).map((propertyName) => {
+    const { aliases, description }: Property = schemaProperties[propertyName];
+    const commandHelp: CommandHelp = {
+      command: `--${propertyName}`,
+      ...(!!aliases && {
+        alias: aliases.map((alias) => "--" + alias).join(", "),
+      }),
+      description: description,
+    };
+
+    return commandHelp;
+  });
+};
+
+const getCommandsHelpFromSchema = (relativePath: string) => {
+  const commandsHelp = generateHelpFromSchema(relativePath);
+  let helpContent = "Available Commands:\n";
+
+  commandsHelp.forEach(({ command, alias, description }) => {
+    helpContent += alias
+      ? `  ${colors.cyan(command)} (${alias}) ${description}\n`
+      : `  ${colors.cyan(command)} ${description}\n`;
+  });
+
+  return helpContent;
+};
+
+export default getCommandsHelpFromSchema;
