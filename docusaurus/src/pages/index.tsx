93ba0ad2842d98eb483b67e09efe4718692a9214
Oskar Zaremba <o.zaremba@codeandpepper.com>
2022-03-15 16:40:11 +0100

    fix: it was not possible to replace all JSX.Element with React.VFC (#160)
    There was an issue mainly with the elements that were imported from external libraries and had return type of JSX.Element

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -4,21 +4,23 @@ import Layout from "@theme/Layout";
 
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
-import Authentication from "../components/Authentication";
-import Architecture from "../components/Architecture";
-import Features from "../components/Features";
-import Hero from "../components/Hero";
+import { Authentication } from "../components/Authentication";
+import { Architecture } from "../components/Architecture";
+import { Features } from "../components/Features";
+import { Hero } from "../components/Hero";
 
-export default function Home(): React.VFC {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout title={siteConfig.tagline} description={siteConfig.tagline}>
-      <Hero />
-      <main>
-        <Features />
-        <Authentication />
-        <Architecture />
-      </main>
-    </Layout>
-  );
-}
+const Home: React.VFC = () => {
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<Layout title={siteConfig.tagline} description={siteConfig.tagline}>
+			<Hero />
+			<main>
+				<Features />
+				<Authentication />
+				<Architecture />
+			</main>
+		</Layout>
+	);
+};
+
+export default Home;
