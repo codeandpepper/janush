58f225fa5bc2d81c710258e2619ffd0258cba37e
Arkadiusz Jelonek <arek.jelonek@gmail.com>
2022-06-08 10:21:01 +0200

    [E2E] Update Playwright to the newest version (#254)
    * Update Playwright to the newest version
    
    * Update version of the janush

--- schematics/web/e2e/framework/utils/playwright/packageJsonExtender.ts
+++ schematics/web/e2e/framework/utils/playwright/packageJsonExtender.ts
@@ -7,12 +7,14 @@ export const packageJsonExtender = (tree: Tree) => {
   mergeJsonFile(tree, `${Schematic.WEB}/package.json`, {
     scripts: {
       "pw:lint": "eslint --ext .ts ./playwright --color",
-      "pw:format": "prettier --write ./playwright/**/*.ts",
-      "pw:open": "playwright test --config ./playwright/config.ts --headed",
-      "pw:debug": "playwright test --config ./playwright/config.ts --debug",
-      "pw:run": "playwright test --config ./playwright/config.ts",
-      "pw:run:firefox": "playwright test --config ./playwright/config.ts --browser=firefox",
-      "pw:run:webkit": "playwright test --config ./playwright/config.ts --browser=webkit",
+      "pw:format": "prettier --write ./playwright/{e2e,page-objects}/**/*.ts",
+      "pw:open": "playwright test --config ./playwright/playwright.config.ts --headed",
+      "pw:debug": "playwright test --config ./playwright/playwright.config.ts --debug",
+      "pw:run": "playwright test --config ./playwright/playwright.config.ts",
+      "pw:run:firefox":
+        "playwright test --config ./playwright/playwright.config.ts --browser=firefox",
+      "pw:run:webkit":
+        "playwright test --config ./playwright/playwright.config.ts --browser=webkit",
     },
   });
 
