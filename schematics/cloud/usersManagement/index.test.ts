69a573c03f47db96eaab4a5bcb7074730778590b
MastalerzKamil <k.mastalerz@codeandpepper.com>
2022-07-12 10:45:21 +0200

    bugfix: reorganize tests file structure and change usersManagement schematic generator

---
+++ schematics/cloud/usersManagement/index.test.ts
@@ -0,0 +1,44 @@
+import { Tree } from "@angular-devkit/schematics";
+import { SchematicTestRunner } from "@angular-devkit/schematics/testing";
+import path from "path";
+
+import expectedJanushFiles from "@janush-schematics/cloud/janush/data/expectedNewFiles.json";
+import expectedTemplateFiles from "@janush-schematics/cloud/template/data/expectedNewFiles.json";
+import expectedUsersManagementFiles from "@janush-schematics/cloud/usersManagement/createUserGroup/data/expectedNewFiles.json";
+import { moduleJanush } from "@mocks/janush";
+import * as janush from "@utility/janushJson";
+
+const collectionPath = path.join(__dirname, "../../collection.json");
+
+describe("cloud.usersManagement", () => {
+  let runner: SchematicTestRunner;
+  beforeEach(() => {
+    runner = new SchematicTestRunner("schematics", collectionPath);
+    jest.spyOn(janush, "readJanushJSON").mockReturnValue(moduleJanush);
+    jest.spyOn(janush, "updateJanushJSON").mockImplementation();
+  });
+
+  it("should generate only users management module", async () => {
+    const templateTree = await runner
+      .runSchematicAsync(
+        "cloud",
+        { name: "janush-app", modules: ["usersManagement"] },
+        Tree.empty(),
+      )
+      .toPromise();
+
+    expect(templateTree.files).toHaveEqualElements([
+      ...expectedJanushFiles,
+      ...expectedTemplateFiles,
+      ...expectedUsersManagementFiles,
+    ]);
+  });
+
+  it("should not generate users management module when not selected", async () => {
+    const templateTree = await runner
+      .runSchematicAsync("cloud", { name: "janush-app", modules: [] }, Tree.empty())
+      .toPromise();
+
+    expect(templateTree.files).not.toContain([...expectedUsersManagementFiles]);
+  });
+});
